6cd032a8dfec24e1a59ecbaa41fb859aa2527654
Spencer Cox <Spencercoxinbox@gmail.com>
2023-09-14 11:38:44 -0600

    made progress on pokemon journey

--- arrays.js
+++ arrays.js
@@ -1,4 +1,71 @@
 /*
-    In this file, we'll be continuing our story
-    with Jon being allowed to flee his fight with Jamie.
+    In this file, we'll be starting a new story about 
+    Doug the pokemon trainer.
 */
+
+let backpack = ["pokeball", "pokeball", 'potion', 'revive', 'running shoes']
+
+let pokemon = []
+
+backpack.push('oreos')
+backpack.push('oreos')
+
+console.log('After buying oreos our backpack looks like this.')
+console.log(backpack)
+
+// Oak gives you a pokedex, and a pikachu
+
+backpack.push('Pokedex')
+pokemon.push('pikachu')
+
+console.log('After meeting professor oak our backpack and team looks like')
+console.log(backpack)
+console.log(pokemon)
+
+// enter route 1
+
+for (let i = 0; i < 4; i++) {
+    pokemon.push('pidgey')
+}
+
+console.log('After finding 4 pidgeys')
+console.log(pokemon)
+
+backpack.splice(4,3)
+
+console.log('after we ate our oreos, we lost our shoes')
+console.log(backpack)
+
+
+
+let newPokemon = 'Charizard'
+// crit capture
+
+pokemon.push(newPokemon);
+
+console.log('Our team now looks like this.')
+console.log(pokemon)
+
+
+//someone asked to see pikachu
+console.log('here is my pikachu!')
+let cutestPokemon = pokemon[0]
+console.log(cutestPokemon);
+
+
+// a scientist asked to clone your pidgeys in exchange for an item
+
+let giftPidgeys = pokemon.slice(1,5)
+console.log('here are your cloned pidgeys.')
+console.log(giftPidgeys)
+
+//making sure your team is still the same
+console.log(pokemon)
+
+backpack.unshift('Big Statue')
+console.log('backpack after the scientists gift')
+console.log(backpack)
+
+
+
+
