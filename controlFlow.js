d8f61aff76e7da76501ecaf6d8927d9c6a9a9b1f
Spencer Cox <Spencercoxinbox@gmail.com>
2023-09-14 12:29:05 -0600

    initial commit

---
+++ controlFlow.js
@@ -0,0 +1,27 @@
+let guessMe = 54
+
+while (guessMe < 100) {
+    if (guessMe % 4 == 0 || guessMe % 5 == 0) {
+        guessMe += 25
+
+        console.log("if was taken")
+        console.log(`guessMe is now ${guessMe}`)
+        console.log("------------------------")
+    } else if (guessMe % 3 == 0) {
+        guessMe -= 27
+
+        console.log("else if was taken")
+        console.log(`guessMe is now ${guessMe}`)
+        console.log("------------------------")
+    } else {
+        guessMe += 3
+
+        console.log("else was taken")
+        console.log(`guessMe is now ${guessMe}`)
+        console.log("------------------------")
+    }
+    guessMe += 22
+}
+
+
+console.log(guessMe)
